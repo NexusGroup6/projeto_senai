9b50b6ee59524bbd5adf35ba474eb8d17cb15d85
vitorbenedito <vitor.bsdc@gmail.com>
2021-09-14 22:07:00 -0300

    componentes que fazem ligação com o arquivo pai App.js

---
+++ pagina-senai/src/componentes/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Footer(){
+    return(
+        <footer>
+            SENAI Suíço-Brasileira | Nexus Group - Copyright 2021
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ pagina-senai/src/componentes/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Header() {
+    return(
+        <header>
+            Escola SENAI Suíço-Brasileira Paulo Ernesto Tolle - (IMG)
+        </header>
+    );
+}
+
+export default Header;
